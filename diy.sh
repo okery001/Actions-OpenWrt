5a1a464f5bc4c3c3eb2155201adec234a61ade1b
okery001 <44427010+okery001@users.noreply.github.com>
2020-02-19 13:10:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#hc5761 lan口转 dw22d
+sed -i 's/"1:lan" "4:lan" "0:wan" "6@eth0"/"1:lan" "2:lan" "3:lan" "4:lan" "0:wan" "6@eth0"/g' target/linux/ramips/mt7620/base-files/etc/board.d/02_network
